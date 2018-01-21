ec8b5cca53477151ace64f76c6cde7f7c5189926
MrDarkman <mrdark6996@gmail.com>
2018-01-22 02:50:25 +0700

    Dddd

--- bot.php
+++ bot.php
@@ -19,6 +19,38 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
+
+			if($text == 'ราคาน้ำมัน'){
+
+				$client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
+		    	array(
+			           "trace"      => 1,		// enable trace to view what is happening
+			           "exceptions" => 0,		// disable exceptions
+			          "cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
+		           );
+
+               $params = array(
+                   'Language' => "en",
+                   'DD' => date('d'),
+                   'MM' => date('m'),
+                   'YYYY' => date('Y')
+               );
+
+		        $data = $client->GetOilPrice($params);
+              $ob = $data->GetOilPriceResult;
+            $xml = new SimpleXMLElement($ob);
+           
+               // PRICE_DATE , PRODUCT ,PRICE
+              foreach ($xml  as  $key =>$val) {  
+				$text = "";
+            if($val->PRICE != ''){
+				$text .= $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
+                }
+
+               }
+				
+			}
+
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
