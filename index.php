e6ce7e07448eca9de1ed5dde6f2f812371fa3868
MrDarkman <mrdark6996@gmail.com>
2018-01-22 03:04:41 +0700

    Dadsasd

--- index.php
+++ index.php
@@ -1,30 +1,37 @@
 <?php
-$client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
-array(
-       "trace"      => 1,		// enable trace to view what is happening
-       "exceptions" => 0,		// disable exceptions
-      "cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
-   );
+    $text = "";
+try{
+    $client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
+    array(
+           "trace"      => 1,		// enable trace to view what is happening
+           "exceptions" => 0,		// disable exceptions
+          "cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
+       );
+    
+    $params = array(
+       'Language' => "en",
+       'DD' => date('d'),
+       'MM' => date('m'),
+       'YYYY' => date('Y')
+    );
+    
+    $data = $client->GetOilPrice($params);
+    $ob = $data->GetOilPriceResult;
+    $xml = new SimpleXMLElement($ob);
+    
+    // PRICE_DATE , PRODUCT ,PRICE
 
-$params = array(
-   'Language' => "en",
-   'DD' => date('d'),
-   'MM' => date('m'),
-   'YYYY' => date('Y')
-);
-
-$data = $client->GetOilPrice($params);
-$ob = $data->GetOilPriceResult;
-$xml = new SimpleXMLElement($ob);
-
-// PRICE_DATE , PRODUCT ,PRICE
-$text = "";
-foreach ($xml  as  $key =>$val) {  
-
-if($val->PRICE != ''){
-$text .=  $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
+    foreach ($xml  as  $key =>$val) {  
+    
+    if($val->PRICE != ''){
+    $text .=  $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
+    }
+    
+    
+    }
+    echo $text;
+}catch (Exception $e) {
+    echo $e->getMessage();
 }
+?>
 
-
-}
-echo $text;
