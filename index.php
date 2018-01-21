a01b101dffea783843ad5d5295287cab17ae5679
MrDarkman <mrdark6996@gmail.com>
2018-01-22 02:55:57 +0700

    Adds

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
-echo "Hello LINE BOT";
+$client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
+array(
+       "trace"      => 1,		// enable trace to view what is happening
+       "exceptions" => 0,		// disable exceptions
+      "cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
+   );
+
+$params = array(
+   'Language' => "en",
+   'DD' => date('d'),
+   'MM' => date('m'),
+   'YYYY' => date('Y')
+);
+
+$data = $client->GetOilPrice($params);
+$ob = $data->GetOilPriceResult;
+$xml = new SimpleXMLElement($ob);
+
+// PRICE_DATE , PRODUCT ,PRICE
+foreach ($xml  as  $key =>$val) {  
+$text = "";
+if($val->PRICE != ''){
+$text .=  'kk' . $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
+}
+
+echo $text;
+}
